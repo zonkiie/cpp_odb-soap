19f3415f1613c5157cb5a61585a8dc11b42445b0
Rainer Datzer <rdatz@gmx.de>
2019-08-16 01:10:30 +0200

    first commit

---
+++ entities.hxx
@@ -0,0 +1,26 @@
+#ifndef __entities__
+#define __entities__
+
+#include <vector>
+#include <string>
+
+#include <odb/core.hxx>
+
+// Include TR1 <memory> header in a compiler-specific fashion. Fall back
+// on the Boost implementation if the compiler does not support TR1.
+//
+#include <odb/tr1/memory.hxx>
+
+using std::tr1::shared_ptr;
+
+
+class user
+{
+};
+
+class domain
+{
+};
+
+#endif
+ 
