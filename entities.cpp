1534e044ee71419073eeb0bdb50f3ad95d320024
Rainer Datzer <rdatz@gmx.de>
2019-08-18 09:12:27 +0200

    working on functions

---
+++ entities.cpp
@@ -0,0 +1,4 @@
+#include <includes.hh>
+
+user::user(){}
+domain::domain(){}
---
+++ soap_functions.cpp
@@ -0,0 +1,15 @@
+#include <includes.hh>
+#include <ns.nsmap> // nsmap is here included because otherwise namespaces will be declared many times.
+
+int ns__getusers(struct soap* soap, vector<string>& userlist)
+{
+	userlist = getUserList();
+	return SOAP_OK;
+}
+
+
+int ns__getUserListDB(struct soap* soap, vector<user>& userlist)
+{
+	//odb::pgsql::database db ("john", "secret", "dummy whammy", "localhost");
+	return SOAP_OK;
+}
